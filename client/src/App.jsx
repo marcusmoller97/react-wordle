18a290d7d33aa825116e0304c799877c070dac27
marcusmoller97 <marcussmoller@icloud.com>
2025-03-27 11:36:10 +0100

    setup App.jsx to server.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+  const [count, setCount] = useState(0);
+
+  const fetchAPI = async () => {
+    const response = await axios.get('http://localhost:5080/');
+    console.log(response.data.fruits);
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
 
   return (
+
     <h1>Hello</h1>
-  )
+
+
+  );
 }
 
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 import express from 'express';
-/* import cors from 'cors'; */
-const app = express();
-const PORT = 5080;
+import cors from 'cors';
 
-app.get('/api', (_req, res) => {
-    res.json({ "fruits": ['apple', 'orange', 'banana'] });
+const PORT = 5080;
+const app = express();
+const corsOptions = {
+    origin: ['http://localhost:5173'],
+}
+
+app.use(cors(corsOptions));
+
+app.get('/', (_req, res) => {
+    res.status(200).send({ 'fruits': ['apple', 'orange', 'banana'] });
 });
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
+    console.log('Press Ctrl+C to quit.');
 });
